6c73d635624434356ada268260a3f559d4278d0b
Suguru Motegi <suguru.motegi@gmail.com>
2017-01-12 10:31:22 -0800

    add save:user:all task

--- gulp/tasks/user.js
+++ gulp/tasks/user.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const path = require('path');
 const gulp = require('gulp');
+const async = require('neo-async');
 
 const madeInJapan = require('../../');
 
@@ -12,4 +14,22 @@ gulp.task('user:save', ['validate'], () => {
     .location(loc)
     .readEngineer(false)
     .saveEngineer();
+});
+
+gulp.task('user:save:all', ['validate'], done => {
+
+  const token = process.env.token;
+  const filepath = path.resolve(__dirname, '..', '..', 'data', 'japan.json');
+  const locations = require(filepath);
+  async.eachSeries(locations, (loc, next) => {
+    console.log(`location:${loc}`);
+    madeInJapan(token)
+      .location(loc)
+      .readEngineer(false)
+      .saveEngineer()
+      .callback(err => {
+        err = /^Only the first 1000/.test(err) ? null : err;
+        next(err);
+      });
+  }, done);
 });

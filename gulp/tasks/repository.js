e6d1006972239d0d6ab0488912d763a1132b171b
Suguru Motegi <suguru.motegi@gmail.com>
2017-04-22 23:23:59 -0700

    udpate gulp task

--- gulp/tasks/repository.js
+++ gulp/tasks/repository.js
@@ -1,12 +1,23 @@
 'use strict';
 
 const gulp = require('gulp');
+const MadeIn = require('../../lib/madeIn');
 const madeInJapan = require('../../');
 
 gulp.task('repository:save', ['validate'], () => {
 
-  const token = process.env.token;
+  const { token } = process.env;
   madeInJapan(token)
+    .readRepository(false)
+    .saveRepository();
+});
+
+gulp.task('repository:ranker:save', ['validate'], () => {
+
+  const { token } = process.env;
+  new MadeIn({ ranker: true })
+    .token(token)
+    .location('Japan')
     .readRepository(false)
     .saveRepository();
 });
